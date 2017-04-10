52fcee923dbb9e35b0537b470d60e67694a9e3d8
Patrick Stapfer <ryyppy@users.noreply.github.com>
2017-04-10 19:38:08 +0200

    Add sound and adapt animation
    This changes the animation duration to ~8 seconds to fit the sounds.
    There are two sounds:
    
    * sound_a -> With echo effect
    * sound_b -> Without echo effect (cleaner)
    * sound_c -> Glenn flavor Nr. 1
    * sound_d -> Glenn flavor Nr. 2
    
    Access `src/constants` to try out the other sound :-)

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Sound from 'react-sound';
 import {Wrapper, Group, BlueText, RedText} from './styles';
+import constants from '../../constants';
 import Logo from '../Logo';
-
 
 const App = () => (
   <Wrapper>
+    <Sound
+      url={constants.sound}
+      playStatus={Sound.status.PLAYING}
+    />
     <Group>
       <BlueText>React</BlueText>
       <Logo width={320} height={320} />
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 const constants = {
-  animationDuration: '1.75s',
+  animationDuration: '6.8s',
   animationEasing: 'ease-in-out',
+  sound: 'sound_b.mp3',
 };
 
 export default constants;
